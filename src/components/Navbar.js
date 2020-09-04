9ed90c0e8dd2497d751180501425101bf6fa4d1e
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-05 00:54:55 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './css/Navbar.css';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Navbar() {
     return (
@@ -23,10 +24,28 @@ function Navbar() {
             </div>
 
             <div className="header__nav">
-                <Link className="header__link" to="login">
+                <Link className="header__link" to="/login">
                     <div className="header__option">
-                        <span>Hello</span>
-                        <span>Sign In</span>
+                        <span className="header__optionLineOne">Hello Nishant</span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+                <Link className="header__link" to="/">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+                <Link className="header__link" to="/">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+                <Link className="header__link" to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount">0</span>
                     </div>
                 </Link>
             </div>

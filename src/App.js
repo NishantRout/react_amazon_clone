9e1f0151bf8dca1c4c8ee83affcfda098ff21b8a
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-15 01:31:17 +0530

    banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Navbar />
+        <Home />
         <Switch>
           <Route path="/checkout" />
           <Route path="/login" />

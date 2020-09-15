7b2f14a029c7f31d898eba82633f5b0861fdf9dc
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-15 15:52:00 +0530

    Subtotal

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Navbar />
-        <Home />
+
         <Switch>
-          <Route path="/checkout" />
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
           <Route path="/login" />
-          <Route path="/" />
+          <Route path="/">
+            <Home />
+          </Route>
         </Switch>
       </div>
     </Router>

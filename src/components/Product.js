d750760a7efeaeb4dd3de964c401596052f2748e
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-09-15 13:15:04 +0530

    Home Page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './css/Product.css';
 
-function Product() {
+function Product(props) {
+    const { title, image, price, rating } = props;
     return (
         <div className="product">
             <div className="product__info">
-                <p>The Lean Startup</p>
+                <p>{title}</p>
                 <p className="product__price">
-                    <small>$</small>
-                    <strong>19.99</strong>
+                    <small>₹</small>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>⭐️</p>
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))}
                 </div>
             </div>
 
             <img
-                className="product__image"
-                src="https://images-na.ssl-images-amazon.com/images/I/81lyNVQ6iXL._SL1500_.jpg"
+                src={image}
                 alt=""
             />
 
